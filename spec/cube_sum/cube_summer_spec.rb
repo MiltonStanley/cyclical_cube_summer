879eb3698219888aefa643647c3abfeab0106b9c
Milton Stanley <milton.stanley@gmail.com>
2018-10-22 13:08:07 -0500

    Searching for triangles good so far

--- spec/cube_sum/cube_summer_spec.rb
+++ spec/cube_sum/cube_summer_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 RSpec.describe CubeSum::CubeSummer do
   let(:triangles) { [153, 370, 371] }
+
   context 'basic functions' do
     it { expect(subject.get_digit_array(125)).to eq([1,2,5]) }
     it { expect(subject.cube_array([1,2,5])).to eq([1,8,125]) }
@@ -20,6 +21,16 @@ RSpec.describe CubeSum::CubeSummer do
       it { expect(subject.single_cycle(9)).to eq(729) }
       it { expect(subject.single_cycle(737)).to eq(713) }
       it { expect(subject.single_cycle(713)).to eq(371) }
+    end
+
+    context 'No other numbers are triangular' do
+      it 'finds no others' do
+        (1..100).each do |integer|
+          next if triangles.include?(integer) || integer = 1
+          actual = subject.single_cycle(integer)
+          expect(actual).not_to eq(integer)
+        end
+      end
     end
   end
 end
